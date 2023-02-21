de9bd2d8b248fe8b604f7bfe5b811ecf26706b8d
cmglmsr <mgulumser@my.aci.k12.tr>
2023-02-21 21:23:55 +0300

    backend init 2

---
+++ backend/src/test/java/app/linkedout/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package app.linkedout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
