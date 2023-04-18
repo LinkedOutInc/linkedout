746989e51e767e67b7bdfa0b8b6db3c03c4fa37d
kagani <m.k4g4n@gmail.com>
2023-04-18 20:58:00 +0300

    [UI] Add/Remove profile stuff

--- www/src/components/pages/profile/interests/index.jsx
+++ www/src/components/pages/profile/interests/index.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import InterestItem from "./interestItem";
+import NewInterest from "./NewInterest";
 
 const Interests = ({ interests }) => {
   return (
     <div className="mt-4 shadow p-6 rounded-2xl">
-      <h1 className="text-2xl font-bold mb-4">Interests</h1>
-      <div className="grid grid-cols-2 gap-4">
+      <div className="flex justify-between">
+        <h1 className="text-2xl font-bold">Interests</h1>
+        <NewInterest />
+      </div>
+      <div className="grid grid-cols-2 gap-4 mt-2">
         {interests.map((interest, index) => (
           <InterestItem key={index} interest={interest} />
         ))}
--- www/src/components/pages/profile/interests/interestItem/index.jsx
+++ www/src/components/pages/profile/interests/interestItem/index.jsx
@@ -7,7 +7,13 @@ const InterestItem = ({ interest }) => {
     <div className="bg-white p-6 shadow rounded flex items-center space-x-4">
       <img src={logoUrl} alt={name} className="w-12 h-12" />
       <div className="flex flex-col">
-        <h2 className="text-xl font-semibold">{name}</h2>
+        <div className="flex justify-between">
+          <h2 className="text-xl font-semibold">{name}</h2>
+          <button className="bg-linkedout text-white font-semibold px-4 rounded-2xl hover:bg-white hover:text-linkedout hover:ring-1 ring-inset hover:ring-linkedout">
+            {" "}
+            -
+          </button>
+        </div>
         <p className="font-light">{area}</p>
       </div>
     </div>
