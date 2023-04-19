868343b0542b4126e73fb7fccf1fc9ffbfdcd887
kagani <m.k4g4n@gmail.com>
2023-04-19 14:59:10 +0300

    [UI] Fix job filters

--- www/src/components/common/header/Authenticated/index.jsx
+++ www/src/components/common/header/Authenticated/index.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Logo from "../../logo";
 import DropdownMenu from "./DropdownMenu";
 
 function Authenticated() {
-  const [isOpen, setIsOpen] = React.useState(false);
+  const navigate = useNavigate();
 
   return (
     <header aria-label="Page Header">
@@ -12,9 +13,9 @@ function Authenticated() {
           <Logo />
           <div class="flex flex-1 items-center justify-between gap-8 sm:justify-end">
             <div class="flex gap-4">
-              <a
-                href="#"
-                class="block shrink-0 rounded-lg bg-white p-2.5 text-gray-600 shadow-sm hover:text-gray-700"
+              <div
+                class="block cursor-pointer shrink-0 rounded-lg bg-white p-2.5 text-gray-600 shadow-sm hover:text-gray-700"
+                onClick={() => navigate("/feed")}
               >
                 <span class="sr-only">Feed</span>
                 <svg
@@ -31,10 +32,10 @@ function Authenticated() {
                     d="M12 7.5h1.5m-1.5 3h1.5m-7.5 3h7.5m-7.5 3h7.5m3-9h3.375c.621 0 1.125.504 1.125 1.125V18a2.25 2.25 0 01-2.25 2.25M16.5 7.5V18a2.25 2.25 0 002.25 2.25M16.5 7.5V4.875c0-.621-.504-1.125-1.125-1.125H4.125C3.504 3.75 3 4.254 3 4.875V18a2.25 2.25 0 002.25 2.25h13.5M6 7.5h3v3H6v-3z"
                   />
                 </svg>
-              </a>
-              <a
-                href="#"
-                class="block shrink-0 rounded-lg bg-white p-2.5 text-gray-600 shadow-sm hover:text-gray-700"
+              </div>
+              <div
+                class="block cursor-pointer shrink-0 rounded-lg bg-white p-2.5 text-gray-600 shadow-sm hover:text-gray-700"
+                onClick={() => navigate("/jobs")}
               >
                 <span class="sr-only">Jobs</span>
                 <svg
@@ -51,11 +52,11 @@ function Authenticated() {
                     d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z"
                   />
                 </svg>
-              </a>
+              </div>
 
-              <a
-                href="#"
-                class="block shrink-0 rounded-lg bg-white p-2.5 text-gray-600 shadow-sm hover:text-gray-700"
+              <div
+                class="block cursor-pointer shrink-0 rounded-lg bg-white p-2.5 text-gray-600 shadow-sm hover:text-gray-700"
+                onClick={() => navigate("/connections")}
               >
                 <span class="sr-only">Connections</span>
                 <svg
@@ -72,7 +73,7 @@ function Authenticated() {
                     d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"
                   />
                 </svg>
-              </a>
+              </div>
             </div>
 
             <DropdownMenu />
