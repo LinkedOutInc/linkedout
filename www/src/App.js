28adbb9aece190bfeaa6100fd768dccaca918765
kagani <m.k4g4n@gmail.com>
2023-06-03 02:09:53 +0300

    [UI] Profile Experience
    NONE OF THIS STACK WAS TESTED

--- www/src/App.js
+++ www/src/App.js
@@ -26,33 +26,35 @@ function App() {
   return (
     <div id="app_container" className="m-0 min-h-screen flex flex-col">
       <AuthProvider>
-        <ProfileProvider>
-          <Header />
-          <div
-            id="app_content"
-            className="my-0 flex flex-1 flex-col float-none"
-          >
-            <Routes>
-              <Route path="/" element={<Landing />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/signup/complete" element={<CompleteSignup />} />
-              <Route element={<AuthRoutes />}>
-                <Route path="/feed" element={<Feed />} />
-                <Route element={<ProfileRoute />}>
-                  <Route path="/profile" element={<Profile />} />
-                </Route>
-                <Route path="/jobs" element={<Jobs />} />
-                <Route path="/recruiter" element={<Recruiter />} />
-                <Route path="/connections" element={<Connections />} />
-                <Route path="/admin/login" element={<AdminLogin />} />
-                <Route path="/admin" element={<Admin />} />
+        <Header />
+        <div id="app_content" className="my-0 flex flex-1 flex-col float-none">
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/signup/complete" element={<CompleteSignup />} />
+            <Route element={<AuthRoutes />}>
+              <Route path="/feed" element={<Feed />} />
+              <Route element={<ProfileRoute />}>
+                <Route
+                  path="/profile"
+                  element={
+                    <ProfileProvider>
+                      <Profile />
+                    </ProfileProvider>
+                  }
+                />
               </Route>
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </div>
-          <Footer />
-        </ProfileProvider>
+              <Route path="/jobs" element={<Jobs />} />
+              <Route path="/recruiter" element={<Recruiter />} />
+              <Route path="/connections" element={<Connections />} />
+              <Route path="/admin/login" element={<AdminLogin />} />
+              <Route path="/admin" element={<Admin />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+        <Footer />
       </AuthProvider>
     </div>
   );
--- www/src/contexts/ProfileContext/index.jsx
+++ www/src/contexts/ProfileContext/index.jsx
@@ -10,17 +10,16 @@ export const useProfile = () => {
 };
 
 const ProfileProvider = ({ children }) => {
+  const API = process.env.REACT_APP_API_URL;
   const { user } = useAuth();
   const { token } = useAuth();
   const [loading, setLoading] = useState(false);
   const [experience, setExperience] = useState([]);
   const [education, setEducation] = useState([]);
   const [interests, setInterests] = useState([]);
-
   const navigate = useNavigate();
 
-  const API = process.env.REACT_APP_API_URL;
-
+  // Education stuff
   const fetchEducation = async () => {
     setLoading((loading) => !loading);
     try {
@@ -102,6 +101,250 @@ const ProfileProvider = ({ children }) => {
     }
   };
 
+  const editEducation = async ({
+    title,
+    description,
+    start_date,
+    end_date,
+    institutionName,
+  }) => {
+    setLoading((loading) => !loading);
+    try {
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      myHeaders.append("Authorization", `Bearer ${token}`);
+
+      var raw = JSON.stringify({
+        experience: {
+          exp_ID: -1, // Doesn't matter
+          user_ID: -1, // Doesn't matter
+          title: title,
+          description: description,
+          type: "EDUCATION",
+          start_date: start_date,
+          end_date: end_date,
+        },
+        institutionName: institutionName,
+      });
+
+      console.log(raw);
+
+      var requestOptions = {
+        method: "PUT",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+      };
+
+      fetch(`${API}/api/v1/experiences/educations`, requestOptions)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Couldn't edit education");
+          }
+          return response.text();
+        })
+        .then((result) => {
+          console.log(result);
+          fetchEducation();
+        })
+        .catch((error) => console.log("error", error));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteEducation = async (exp_ID) => {
+    setLoading((loading) => !loading);
+    try {
+      var myHeaders = new Headers();
+      myHeaders.append("Authorization", `Bearer ${token}`);
+
+      var requestOptions = {
+        method: "DELETE",
+        headers: myHeaders,
+        redirect: "follow",
+      };
+
+      fetch(`${API}/api/v1/experiences/educations/${exp_ID}`, requestOptions)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Couldn't delete education");
+          }
+          return response.text();
+        })
+        .then((result) => {
+          console.log(result);
+          fetchEducation();
+        })
+        .catch((error) => console.log("error", error));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  // Experience stuff
+  const fetchExperience = async () => {
+    setLoading((loading) => !loading);
+    try {
+      var myHeaders = new Headers();
+      myHeaders.append("Authorization", `Bearer ${token}`);
+
+      var requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
+      };
+
+      fetch(`${API}/api/v1/experiences`, requestOptions)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Couldn't fetch experience");
+          }
+          return response.text();
+        })
+        .then((result) => {
+          setExperience(() => JSON.parse(result));
+          setLoading((loading) => !loading);
+        })
+        .catch((error) => console.log("error", error));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const addExperience = async ({
+    title,
+    description,
+    start_date,
+    end_date,
+    companyName,
+  }) => {
+    setLoading((loading) => !loading);
+    try {
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      myHeaders.append("Authorization", `Bearer ${token}`);
+
+      var raw = JSON.stringify({
+        experience: {
+          exp_ID: -1, // Doesn't matter
+          user_ID: -1, // Doesn't matter
+          title: title,
+          description: description,
+          type: "EXPERIENCE",
+          start_date: start_date,
+          end_date: end_date,
+        },
+        companyName: companyName,
+      });
+
+      console.log(raw);
+
+      var requestOptions = {
+        method: "POST",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+      };
+
+      fetch(`${API}/api/v1/experiences`, requestOptions)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Couldn't add experience");
+          }
+          return response.text();
+        })
+        .then((result) => {
+          console.log(result);
+          fetchExperience();
+        })
+        .catch((error) => console.log("error", error));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const editExperience = async ({
+    title,
+    description,
+    start_date,
+    end_date,
+    companyName,
+  }) => {
+    setLoading((loading) => !loading);
+    try {
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "application/json");
+      myHeaders.append("Authorization", `Bearer ${token}`);
+
+      var raw = JSON.stringify({
+        experience: {
+          exp_ID: -1, // DOES MATTER
+          user_ID: -1, // Doesn't matter
+          title: title,
+          description: description,
+          type: "EXPERIENCE",
+          start_date: start_date,
+          end_date: end_date,
+        },
+        companyName: companyName,
+      });
+
+      console.log(raw);
+
+      var requestOptions = {
+        method: "PUT",
+        headers: myHeaders,
+        body: raw,
+        redirect: "follow",
+      };
+
+      fetch(`${API}/api/v1/experiences`, requestOptions)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Couldn't edit experience");
+          }
+          return response.text();
+        })
+        .then((result) => {
+          console.log(result);
+          fetchExperience();
+        })
+        .catch((error) => console.log("error", error));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const deleteExperience = async (exp_ID) => {
+    setLoading((loading) => !loading);
+    try {
+      var myHeaders = new Headers();
+      myHeaders.append("Authorization", `Bearer ${token}`);
+
+      var requestOptions = {
+        method: "DELETE",
+        headers: myHeaders,
+        redirect: "follow",
+      };
+
+      fetch(`${API}/api/v1/experiences/${exp_ID}`, requestOptions)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Couldn't delete experience");
+          }
+          return response.text();
+        })
+        .then((result) => {
+          console.log(result);
+          fetchExperience();
+        })
+        .catch((error) => console.log("error", error));
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   const value = {
     user,
     loading,
@@ -113,6 +356,12 @@ const ProfileProvider = ({ children }) => {
     setInterests,
     fetchEducation,
     addEducation,
+    editEducation,
+    deleteEducation,
+    fetchExperience,
+    addExperience,
+    editExperience,
+    deleteExperience,
   };
 
   return (
