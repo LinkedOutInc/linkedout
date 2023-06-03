0969739a13c0bfe703ba37ac840027ca727196d9
kagani <m.k4g4n@gmail.com>
2023-06-03 16:45:27 +0300

    [UI] Dropdown fix
    Wasted an hour on this.

--- www/src/routes/AuthRoute/index.jsx
+++ www/src/routes/AuthRoute/index.jsx
@@ -1,15 +1,16 @@
 import { Outlet, Navigate } from "react-router-dom";
 import { useAuth } from "../../contexts/AuthContext";
-import { NotFound } from "../../components";
+import { Logo, NotFound } from "../../components";
 
 function UserRoutes() {
   const { user } = useAuth();
 
   return user === null ? (
-    <div role="status" className="flex justify-center mx-auto">
+    <div role="status" className="flex flex-col justify-center mx-auto">
+      <Logo className={"text-2xl "} />
       <svg
         aria-hidden="true"
-        class="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        class="mt-12 mr-2 ml-8 w-8 h-8 text-gray-200 animate-spin dark:text-gray-200 fill-linkedout"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
