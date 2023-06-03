52d53c134710f0fcb17fb6d17b32738ad31784f1
Alp Afyonluoğlu <alpafyonluoglu@gmail.com>
2023-06-03 15:28:30 +0300

    Error handling

---
+++ backend/src/main/java/app/linkedout/backend_v2/dto/Error.java
@@ -0,0 +1,25 @@
+package app.linkedout.backend_v2.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.http.ResponseEntity;
+
+import java.util.HashMap;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class Error extends HashMap<String, Object> {
+    private int status;
+    private String message;
+
+    private Error(int status, String message) {
+        put("status", status);
+        put("text", message);
+    }
+
+    public static ResponseEntity create(int status, String message) {
+        return ResponseEntity.status(status).body(new Error(status, message));
+    }
+}
