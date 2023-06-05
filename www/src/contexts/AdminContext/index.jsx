1f5487a302b636210637d343fac9852aa2d6c1ac
kagani <m.k4g4n@gmail.com>
2023-06-05 21:29:52 +0300

    [UI] Admin Provider

---
+++ www/src/contexts/AdminContext/index.jsx
@@ -0,0 +1,95 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../AuthContext";
+
+const AdminContext = React.createContext();
+
+export const useAdmin = () => {
+  return React.useContext(AdminContext);
+};
+
+const AdminProvider = ({ children }) => {
+  const API = process.env.REACT_APP_API_URL;
+  const { user } = useAuth();
+  const { token } = useAuth();
+  const [loading, setLoading] = useState(false);
+  const [roleCountReport, setRoleCountReport] = useState({});
+  const [hiringReport, setHiringReport] = useState({});
+  const navigate = useNavigate();
+
+  const generateRoleCountReport = async () => {
+    setLoading(true);
+    try {
+      var myHeaders = new Headers();
+      myHeaders.append("Authorization", `Bearer ${token}`);
+
+      var requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
+      };
+
+      fetch(`${API}/admin/generateRoleCountReport`, requestOptions)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.text();
+        })
+        .then((result) => {
+          console.log(result);
+          setRoleCountReport(() => JSON.parse(result));
+        })
+        .catch((error) => console.log("error", error));
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+    return roleCountReport;
+  };
+
+  const generateHiringReport = async () => {
+    setLoading(true);
+    try {
+      var myHeaders = new Headers();
+      myHeaders.append("Authorization", `Bearer ${token}`);
+
+      var requestOptions = {
+        method: "GET",
+        headers: myHeaders,
+        redirect: "follow",
+      };
+
+      fetch(`${API}/admin/generateHiringReport`, requestOptions)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.text();
+        })
+        .then((result) => {
+          console.log(result);
+          setHiringReport(() => JSON.parse(result));
+        })
+        .catch((error) => console.log("error", error));
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+    return hiringReport;
+  };
+
+  const value = {
+    user,
+    loading,
+    generateHiringReport,
+    generateRoleCountReport,
+  };
+
+  return (
+    <AdminContext.Provider value={value}>{children}</AdminContext.Provider>
+  );
+};
+
+export { AdminProvider };
